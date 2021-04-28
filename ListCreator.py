a557da28da144aeb4e45e6fe2f84b2aa20a4937c
JackTYM <50038825+JackTYM@users.noreply.github.com>
2021-04-28 16:21:57 -0700

    LineCreator Updates
    Added 1 command line usage
    
    eg. python ListCreator.py x "print(x)" 1 letters.txt

--- ListCreator.py
+++ ListCreator.py
@@ -47,6 +47,12 @@ def list_creator():
 							if list:
 								for letter in list:
 									print(format.replace(placeholder, letter))
+								print('Task is finished')
+								print("Press the 'Enter' key to close the window")
+								close = input()
+								
+								if close:
+									sys.exit()
 						elif mode == '2':
 							while(True):
 								print("Input your text file's name and extention or the filepath")
@@ -65,6 +71,12 @@ def list_creator():
 											break
 										print(format.replace(placeholder, line.strip()))
 									break
+								print('Task is finished')
+								print("Press the 'Enter' key to close the window")
+								close = input()
+								
+								if close:
+									sys.exit()
 				elif output == '2':
 						print('Select your mode:')
 						print('(1) List mode (each letter of the list is used instead of', placeholder, ')')
@@ -74,7 +86,7 @@ def list_creator():
 						if mode:
 							if mode == '1':
 								print('Input your list:')
-								list = input()
+								file = input()
 
 								if list:
 									try:
@@ -84,9 +96,21 @@ def list_creator():
 									finally:
 										with open('ListCreator.txt', 'w') as fl:
 											pass
-											for letter in list:
+											for letter in file:
 												fl.write(format.replace(placeholder, letter))
 												fl.write('\n')
+											print('Task is finished')
+											print("Press the 'Enter' key to close the window")
+											close = input()
+								
+											if close:
+												sys.exit()
+
+											if returning == ('1'):
+												os.system('cls')
+												import JTools
+											else:
+												sys.exit()
 							elif mode == '2':
 								while(True):
 									print("Input your text file's name and extention or the filepath")
@@ -109,7 +133,55 @@ def list_creator():
 													fl.write(format.replace(placeholder, line.strip()))
 													fl.write('\n')
 									break
+								print('Task is finished')
+								print("Press the 'Enter' key to close the window")
+								close = input()
+								
+								if close:
+									sys.exit()
 	except:
 		err()
 
-list_creator()
+
+if len(sys.argv) == 1:
+	list_creator()
+elif len(sys.argv) == 5:
+	placeholder = sys.argv[1]
+	format = sys.argv[2]
+	output = sys.argv[3]
+	file = sys.argv[4]
+
+	if placeholder in format:
+		if output == ('1'):
+			try:
+				try: 
+					x = open(file, 'r')
+				except:
+					print('The file seems to be missing or cannot be opened. Try again or drag the file into your command window')
+				while(True):
+					line = x.readline()
+
+					if not line:
+						break
+					print(format.replace(placeholder, line.strip()))
+			except: print('One or more of your arguments are invalid. Try again or use the GUI (Run without arguments)')
+		elif output == ('2'):
+			try:
+				try:
+					os.remove('ListCreator.txt')
+				except: pass
+								
+				finally:
+					with open('ListCreator.txt', 'w') as fl:
+						pass
+						for letter in file:
+							fl.write(format.replace(placeholder, letter))
+							fl.write('\n')
+			except: print('One or more of your arguments are invalid. Try again or use the GUI (Run without arguments)')
+		else: print("Output is not '1' or '2.' Try again or use the GUI (Run without arguments)")
+	else: print(placeholder, "is not included in", format)
+else:
+	print("Error while parsing arguments")
+	print("Please use the following format:")
+	print("python ListCreator.py (placeholder) (format) (output mode) (filepath to txt file)")
+	print("Or use the GUI for more options (Run without arguments)")
